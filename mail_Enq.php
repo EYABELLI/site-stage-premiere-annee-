a60b2cbd31da05f02c27c1de3e5faf72e9bc57f0
EYABELLI <83172479+EYABELLI@users.noreply.github.com>
2022-09-24 12:08:23 +0100

    Add files via upload

---
+++ mail_Enq.php
@@ -0,0 +1,6 @@
+ Nous vous remercions de votre implication dans notre démarche qualité, votre participation permettra à
+       l’équipe de Laboratoire 
+	   BIO DHAOUADI d’améliorer la qualité de ses services pour mieux vous satisfaire. 
+    
+
+	
